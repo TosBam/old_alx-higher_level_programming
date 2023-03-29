d35e03a43ba8bb00a6228b36cb01926991db7a9a
TosBam <samuelson.os19@gmail.com>
2023-03-29 13:36:15 -0700

    class task in OOP python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+"""Square module definition.
+This module defines a simple `Square` class
+"""
 
 
 class Square:
-    """Empty class Square that defines a square"""
+    """The simplest ``Square`` class
+    For now the class has neither ``Atribures`` nor ``Methods``
+    """
     pass
-
