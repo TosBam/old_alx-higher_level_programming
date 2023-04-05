0da3cbf14acb180096844c7064fb70ec5ddf6a09
TosBam <samuelson.os19@gmail.com>
2023-04-05 08:13:07 -0700

    The third task

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,13 +14,13 @@ class Rectangle:
 
     def area(self):
         """ area """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """ perimeter"""
-        if self.width = 0 or self.height = 0:
+        if self.__width = 0 or self.__height = 0:
             return 0
-        return 2(self.width + self.height)
+        return 2(self.__width + self.__height)
 
     @property
     def width(self):
