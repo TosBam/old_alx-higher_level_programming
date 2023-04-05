a380907f61c12fc0f8274d74cf10de11e8c18a68
TosBam <samuelson.os19@gmail.com>
2023-04-05 09:13:39 -0700

    Task 3, appendation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,5 +53,13 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-
+    def __str__(self):
+        """the printable version"""
+        if (self.__width == 0 or self.__height == 0):
+            return ("")
+        last = []
+        for i in range(self.__height):
+            [last.append('#') for j in range(self.__width)]
+            if i != (self.__height - 1):
+                last.append('\n')
+        return ("".join(last))
