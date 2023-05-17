a820d7066d4515b6aa738930cf522c32c3157f51
TosBam <samuelson.os19@gmail.com>
2023-05-17 08:16:53 -0700

    All about table and list

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -2,4 +2,4 @@
 -- database in your MySQL server.
 -- The database name will be passed as argument of mysql command
 -- (in the following example: mysql is the name of the database).
-SHOW TABLES FROM mysql;
+SHOW TABLES;
